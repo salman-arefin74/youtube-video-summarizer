919b13c48bd559be3dd3d5e604f8e83c14b14348
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-08-27 22:41:15 +0200

    Update and rename transcript-loader.py to transcript_loader.py

--- transcript_loader.py
+++ transcript_loader.py
@@ -12,20 +12,8 @@ def get_video_id(url: str) -> str:
 
 def fetch_transcript(url: str, lang="en"):
     video_id = get_video_id(url)
-    print("video id: ", video_id)
     ytt_api = YouTubeTranscriptApi()
     transcript = ytt_api.fetch(video_id)
-    # print("transcript: ", transcript)
     cleaned_snippets = [re.sub(r'\s+', ' ', snippet.text.strip()) for snippet in transcript.snippets]
-    
-    # Join into a single string
     transcript_text = " ".join(cleaned_snippets)
     return transcript_text
-
-# Example usage
-url = "https://www.youtube.com/watch?v=_K-eupuDVEc&ab_channel=IGotAnOffer%3AEngineering"
-try:
-    text = fetch_transcript(url)
-    print(text)
-except Exception as e:
-    print("Error:", e)
